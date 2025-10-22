0724942e6db5711783a30cef0bc5930727bf9dee
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-22 21:48:59 +0100

    feat: setup server and add tsconfig.json

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 
+import { PORT } from './constants/app';
+
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
