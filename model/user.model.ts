f357cb08304cfe065b3298ceea7a6c482112e620
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-24 22:49:01 +0100

    feat: create the user model

---
+++ model/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
