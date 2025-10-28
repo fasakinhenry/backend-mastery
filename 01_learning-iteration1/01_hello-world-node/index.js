a5a95a8ee0b74739f213619c9d0dbd7d95aa3d70
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-28 07:31:43 +0100

    feat: add code for the hello world

---
+++ 01_learning-iteration1/01_hello-world-node/index.js
@@ -0,0 +1,11 @@
+console.log('hello node js');
+
+const array = [1, 2, 3, 4];
+
+console.log('Array contents:', array);
+
+setTimeout(() => {
+  console.log('This message is displayed after 2 seconds');
+}, 2000);
+
+console.log('This is the last line of the synchronous code');
