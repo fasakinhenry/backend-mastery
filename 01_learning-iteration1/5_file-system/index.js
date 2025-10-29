70eb5ea30af4d1aa2c93af5eced3bfc4048b6613
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-29 09:15:31 +0100

    feat: add asynchronous method conterparts for fs module

--- 01_learning-iteration1/5_file-system/index.js
+++ 01_learning-iteration1/5_file-system/index.js
@@ -12,12 +12,36 @@ if (!fs.existsSync(dataFolder)) {
 const filePath = path.join(dataFolder, 'example.txt');
 
 // Create file using the synchronous method
-fs.writeFileSync(filePath, "Hello from Node.js!");
+fs.writeFileSync(filePath, 'Hello from Node.js!');
 console.log('File created successfully');
 
 const readContentFromFile = fs.readFileSync(filePath, 'utf-8');
 console.log('File content:', readContentFromFile);
 
 // Append one more line
-fs.appendFileSync(filePath, "\nThis is a new line appended to this file.");
+fs.appendFileSync(filePath, '\nThis is a new line appended to this file.');
 console.log('New file content added');
+
+// Use the async methods
+const asyncFilePath = path.join(dataFolder, 'async-example.txt');
+
+fs.writeFile(asyncFilePath, 'Hello, Async Node.js!', (err) => {
+  if (err) throw err;
+  console.log('Async file created successfully');
+  fs.readFile(asyncFilePath, 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log('Async file content:', data);
+    fs.appendFile(
+      asyncFilePath,
+      '\nThis is a new line appended asynchronously',
+      (err) => {
+        if (err) throw err;
+        console.log('New line added to async file');
+        fs.readFile(asyncFilePath, 'utf-8', (err, updatedData) => {
+            if (err) throw err;
+            console.log('Updated async file content:', updatedData);
+        });
+      }
+    );
+  });
+});
