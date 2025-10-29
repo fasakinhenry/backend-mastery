7a28bae7f71a0026b7a2a7ed47ee843adf42d9e9
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-29 17:54:57 +0100

    feat: add file editing to demonstrate callback hell

---
+++ 01_learning-iteration1/7_callbacks/callback-hell.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    const modifyFileData = data.toUpperCase();
+    fs.writeFile('output.txt', modifyFileData, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            return;
+        }
+        console.log('File written successfully');
+
+        fs.readFile('output.txt', 'utf-8', (err, newData) => {
+            if (err) {
+                console.error('Error reading file:', err);
+                return;
+            }
+            console.log('New file content:', newData);
+        });
+    });
+}); 
