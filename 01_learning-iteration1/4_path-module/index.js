6998d65fad8e2578686da89394159ba859041b1b
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-29 08:46:43 +0100

    feat: add index.js to explain path modules

---
+++ 01_learning-iteration1/4_path-module/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+console.log('Current directory:', path.dirname(__filename));
+console.log('File name:', path.basename(__filename));
+console.log('File extension:', path.extname(__filename));
+
+const joinPath = path.join('/user', 'documents', 'node', 'projects');
+console.log('Joined path:', joinPath);
+
+const resolvePath = path.resolve('user', 'documents', 'node', 'projects');
+console.log('Resolved path:', resolvePath);
+
+const normalizePath = path.normalize('/user/.documents/../node//projects');
+console.log('Normalized path:', normalizePath);
+
