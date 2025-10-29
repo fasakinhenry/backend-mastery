b18ddcda78d34d62f4752f88fd13a20ce717fcbb
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-29 20:03:35 +0100

    feat: add promise with resolve and reject

--- 01_learning-iteration1/8_promises/index.js
+++ 01_learning-iteration1/8_promises/index.js
@@ -5,4 +5,19 @@ function delayFn(time) {
 console.log('Promise lecture starts');
 
 delayFn(2000).then(() => console.log('After 2 seconds promise resolves'));
-console.log("Promise ends");
+console.log('Promise ends');
+
+// Create a new promise with resolve and reject
+function divideFn(num1, num2) {
+  return new Promise((resolve, reject) => {
+    if (num2 === 0) {
+      reject('Cannot perform division by zero');
+    } else {
+      resolve(num1 / num2);
+    }
+  });
+}
+
+divideFn(10, 5)
+  .then((result) => console.log('Result', result))
+  .catch((error) => console.log('Error:', error));
