13d0f5f59adeb58e1d54c46cdf3a23c38ccffd95
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-02 22:50:46 +0100

    feat: add all the query examples

--- 01_learning-iteration1/14_mongodb-basics/app.js
+++ 01_learning-iteration1/14_mongodb-basics/app.js
@@ -36,6 +36,43 @@ async function runQueryExamples() {
     // });
     // await newUser.save();
     console.log('Created new user', newUser);
+    // const allUsers = await User.find({});
+
+    // console.log('All Users:', allUsers);
+
+    // const getUserOfActiveFalse = await User.find({ isActive: false });
+    // console.log(getUserOfActiveFalse);
+
+    // const getJohnDoeUser = await User.findOne({ name: 'John Doe' });
+    // console.log(getJohnDoeUser);
+
+    // const getLastCreatedUser = await User.findById(newUser._id);
+    // console.log(getLastCreatedUser);
+
+    // const selectedFields = await User.find().select('name email -_id');
+    // console.log(selectedFields)
+
+    // const limitedUser = await User.find().limit(3).skip(1);
+    // console.log(limitedUser);
+
+    // const sortedUsers = await User.find().sort({ age: -1 });
+    // console.log(sortedUsers);
+
+    // const countDocuments = await User.countDocuments({ isActive: true });
+    // console.log('Count of active users:', countDocuments);
+
+    // const deletedUser = await User.findByIdAndDelete(newUser._id);
+    // console.log('Deleted User:', deletedUser);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      newUser._id,
+      {
+        $set: { age: 100 },
+        $push: { tags: 'mentor' },
+      },
+      { new: true }
+    );
+    console.log('Updated User:', updatedUser);
   } catch (error) {
     console.log('Error', error);
   } finally {
