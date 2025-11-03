6ff4a6a92ce2356e3684a062fb3b0442bc105fd8
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-03 21:45:31 +0100

    feat(setup): setup routes for books

---
+++ 01_learning-iteration1/15_bookstore-api/routes/book-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Create express router
+const router = express.Router();
+
+// All the routes related to books only
+router.get('/get',)
+router.get('/get/:id',)
+router.post('/add',)
+router.put('/update/:id',)
+router.delete('/delete/:id',)
