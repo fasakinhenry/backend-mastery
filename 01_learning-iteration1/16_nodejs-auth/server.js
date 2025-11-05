c8a28a9f2f85abd2975b3f018f8ccfcc42ed9fda
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-05 16:37:54 +0100

    feat: create the user model

--- 01_learning-iteration1/16_nodejs-auth/server.js
+++ 01_learning-iteration1/16_nodejs-auth/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const connectToDB = require('./database/db')
 
 const app = express();
 
 app.use(express.json());
+
+// Connect to database
+connectToDB()
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
