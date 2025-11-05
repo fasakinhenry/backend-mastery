cb681b37c78723bc81fb060e9765d51ceeed3d8a
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-05 16:44:50 +0100

    feat: create the auth controllers

---
+++ 01_learning-iteration1/16_nodejs-auth/controllers/auth-controller.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+
+// Register controller
+const registerUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong. Please try again',
+    });
+  }
+};
+
+// Login controller
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong. Please try again',
+    });
+  }
+};
+
+module.exports = {
+    registerUser,
+    loginUser
+}
