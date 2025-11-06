e15d7298781b57342de3e31550287fa6365783cb
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 13:59:57 +0100

    feat: create and use the auth middleware to protect home route

--- 01_learning-iteration1/16_nodejs-auth/routes/home-routes.js
+++ 01_learning-iteration1/16_nodejs-auth/routes/home-routes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth-middleware');
 
 router.get('/welcome', authMiddleware, (req, res) => {
+  const { userId, username, role } = req.userInfo;
   res.json({
     message: 'Welcome to the home page',
+    user: {
+        _id: userId,
+        username,
+        role
+    }
   });
 });
 module.exports = router;
