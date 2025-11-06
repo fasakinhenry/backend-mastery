38d6199e1b0713894c570f54846a05b9f11fabe3
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 13:34:22 +0100

    feat: create a simple admin route

---
+++ 01_learning-iteration1/16_nodejs-auth/routes/admin-routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/home', (req, res) => {
+  res.json({
+    message: 'Welcome to the Admin page',
+  });
+});
+module.exports = router;
