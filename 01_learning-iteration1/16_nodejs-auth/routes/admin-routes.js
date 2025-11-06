84e7188664998ab6455fd8beb3c106514a8c9a12
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 14:09:46 +0100

    feat: create the admin middleware

--- 01_learning-iteration1/16_nodejs-auth/routes/admin-routes.js
+++ 01_learning-iteration1/16_nodejs-auth/routes/admin-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth-middleware');
+const isAdminUser = require('../middleware/admin-middleware');
 
-router.get('/welcome', authMiddleware, (req, res) => {
+router.get('/welcome', authMiddleware, isAdminUser, (req, res) => {
   res.json({
     message: 'Welcome to the Admin page',
   });
