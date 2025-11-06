4c6901fc7a68866a86aa9e34ec9a2b7c4bd521ce
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 13:30:25 +0100

    feat: create the registration and login auth controllers

--- 01_learning-iteration1/16_nodejs-auth/controllers/auth-controller.js
+++ 01_learning-iteration1/16_nodejs-auth/controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Register controller
 const registerUser = async (req, res) => {
@@ -56,6 +57,47 @@ const registerUser = async (req, res) => {
 // Login controller
 const loginUser = async (req, res) => {
   try {
+    // Collect username and password
+    const { username, password } = req.body;
+
+    // Find if the current user exists in database
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: `User doesn't exist`,
+      });
+    }
+
+    // Check if password is correct or not
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid credentials!',
+      });
+    }
+
+    // Create a token
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: '15m',
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: 'Logged in successfully',
+      accessToken,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
